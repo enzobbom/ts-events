98498691526fdacf7c61bbeba9770014b6774c76
Enzo Bom <enzobom@gmail.com>
2026-06-19 18:31:17 +0100

    Add Messaging classes

---
+++ src/main/java/com/javanauta/ts/events/messaging/Exchanges.java
@@ -0,0 +1,7 @@
+package com.javanauta.ts.events.messaging;
+
+public final class Exchanges {
+    public static final String NOTIFICATION = "notification.exchange";
+
+    private Exchanges() {}
+}
---
+++ src/main/java/com/javanauta/ts/events/messaging/RoutingKeys.java
@@ -0,0 +1,8 @@
+package com.javanauta.ts.events.messaging;
+
+public final class RoutingKeys {
+    public static final String NOTIFICATION_REQUEST = "notification.request";
+    public static final String NOTIFICATION_COMPLETED = "notification.completed";
+
+    private RoutingKeys() {}
+}
