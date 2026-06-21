3e0b5428cc77262160f027a3baccf6965c33b112
Enzo Bom <enzobom@gmail.com>
2026-06-21 12:41:09 +0100

    Add Queues constants

---
+++ src/main/java/com/javanauta/ts/events/messaging/Queues.java
@@ -0,0 +1,8 @@
+package com.javanauta.ts.events.messaging;
+
+public final class Queues {
+    public static final String NOTIFICATION_REQUEST = "notification.request.queue";
+    public static final String NOTIFICATION_COMPLETED = "notification.completed.queue";
+
+    private Queues() {}
+}
